667e2d8302138ea1c43369d78e6a3d129138b448
Henry Doan <henrydoan96@gmail.com>
2021-04-12 20:06:15 -0600

    end of lecture

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+import { Grid, Input, Button } from 'semantic-ui-react';
+import axios from 'axios';
+import Tweets from './Tweets';
 
-function App() {
+const App = () => {
+  const [tweets, setTweets] = useState([])
+  const [visible, setVisible] = useState([])
+  const [search, setSearch] = useState("")
+  const [tweet, setTweet] = useState("")
+
+  useEffect( () => {
+    axios.get('/api/tweets')
+      .then( res => setTweets(res.data) )
+      .catch( err => console.log(err) )
+  }, [])
+
+  const handleChange = (e) => {
+    setSearch(e.target.value)
+    updateVisible()
+  }
+
+  const updateVisible = () => {
+    if (search.length === 0) {
+      setVisible(tweets)
+    } else if ( search.length > 3) {
+      axios.get(`/api/search?term=${search}`)
+        .then( res => setVisible( res.data ) )
+        .catch( err => console.log(err) )
+    }
+  }
+
+  const updateTweet = (e) => {
+    setTweet(e.target.value)
+  }
+
+  const postTweet = () => {
+    if (tweet) {
+      axios.post('/api/tweet', { tweet })
+      .then(res => {
+        setTweet('')
+        setTweets([...visible, res.data])
+      })
+      .catch( err => console.log(err) )
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Grid>
+      <Grid.Row>
+        <Grid.Column mobile={16} tablet={16} computer={4}>
+          <h2>Search</h2>
+          <Input
+            value={search}
+            onChange={handleChange}
+            placeholder="Search..."
+            icon={{ name: 'search', circular: true }}
+          />
+          <hr />
+          <h1>Tweet something</h1>
+          <Input onChange={updateTweet} value={tweet} />
+          {/* <Input onChange={(e, value) => setTweet(value)} value={tweet} /> */}
+          <Button onClick={postTweet}>Tweet !</Button>
+        </Grid.Column>
+        <Grid.Column mobile={16} tablet={16} computer={10}>
+          <>
+            <h1>Tweets</h1>
+            <Tweets tweets={visible} />
+          </>
+        </Grid.Column>
+      </Grid.Row>
+    </Grid>
+  )
 }
-
 export default App;
